4733b29c47e91a9d2a964c71919700bb774fd33e
neta butensky <netabutensky@gmail.com>
2019-08-20 14:39:16 +0300

    newone

--- vlogy-front-end/src/Components/Landing.js
+++ vlogy-front-end/src/Components/Landing.js
@@ -1,27 +1,34 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
-
+import { Link } from 'react-router-dom';
+import Login from './Login';
+import Signup from './Signup'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class Landing extends Component {
     constructor() {
         super()
         this.state = {
-           
-           
+
+
         }
     }
 
-   
+
 
     render() {
-  
+
 
         return (
-            
-
 
             <div >
-        </div>
+                <Router>
+                    <Route path="/" exact render={() => <Login />} />
+                    <Route path="/signup" exact render={() => <Signup />} />
+                    
+                </Router>
+
+
+            </div>
 
 
         )
