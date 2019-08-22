f8f74c4f4adfa48eea2ad8b40122f576c4be18f8
nimrod silberfeld <nimrod513@gmail.com>
2019-08-22 11:21:39 +0300

    update4

--- vlogy-front-end/src/Components/VideoUserP.js
+++ vlogy-front-end/src/Components/VideoUserP.js
@@ -8,22 +8,59 @@ class VideoUserP extends Component {
         super()
         this.state = {
             like: 0,
-            likes: false
+            likes: false,
+            timeclick: 0
         }
     }
 
 
+    postLike = () => {
+        if (this.state.like == 0) {
 
-    likes = () => {
-        this.setState({
-            likes: true,
-            like: this.props.d.likes + 1
-        }, function () {
-            this.updatelikes()
-        })
+            this.setState({
+                likes: true,
+                like: this.props.d.likes + 1,
 
+
+            }, function () {
+                this.updatelikes()
+            })
+        }
+        else if (this.state.like < 2) {
+            this.setState({
+                like:this.props.d.like==0
+            })
+        }
+        else if(this.state.like>1){
+            this.setState({
+                likes: true,
+                like: this.props.d.likes - 1,
+
+
+            }, function () {
+                this.updatelikes()
+            })
+        }
+        // else if (this.state.like > 0 && this.state.timeclick > 0) {
+        //     this.setState({
+        //         likes: true,
+        //         like: this.props.d.likes - 1,
+
+        //     }, function () {
+        //         this.updatelikes()
+        //     })
+        // }
+        // else if (this.state.like < 1) {
+        //     this.setState({
+        //         likes: true,
+        //         like: this.props.d.likes + 1,
+
+
+        //     }, function () {
+        //         this.updatelikes()
+        //     })
+        // }
     }
-
 
     updatelikes = () => {
         let datas = this.props.updateUserVideo({ data: this.state.like, prop: 'likes', username: this.props.username, videoId: this.props.d.videoId })
@@ -44,11 +81,13 @@ class VideoUserP extends Component {
                         </div>
                     </div>
                     <div className="card4">
-                        <div onClick={this.likes} className="container">
+                        <div onClick={this.postLike} className="container">
                             <i class="fas fa-heart"></i>
-                            {this.state.likes ? this.props.d.likes + 1 : this.props.d.likes}
-
+                            {this.state.likes ? this.props.d.likes : this.props.d.likes}
                         </div>
+                        {/* <div><button onClick={this.unlike}>unlike</button>
+
+                        </div> */}
                     </div>
                     <video className="videoss" width="400" height="300" controls>
                         <source src={`http://localhost:5000/video/${this.props.d.videoId}`} />
